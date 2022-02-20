9e0943ea119e2db6af22c8d7cbff72c03aa03fbf
kallifabio <34612468+kallifabio@users.noreply.github.com>
2022-02-20 15:48:07 +0100

    fix

--- scoreboard.php
+++ scoreboard.php
@@ -66,8 +66,14 @@ require_once 'mysql/connection.php'
       </div>
     </div>
   </footer>
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <!--JavaScript at end of body for optimized loading-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('.sidenav').sidenav();
+    });
+  </script>
 </body>
 
 </html>
